2388d39ba01179982c71d2e39a5f07419ce83f66
Brayan <brayan@nexus.com>
2025-10-10 14:24:46 -0400

    🚀 Deploy GitHub Pages - NEXUS
    Site compilado e pronto para GitHub Pages
    
    Build info:
    - index.html: 0.77 kB
    - CSS: 109.12 kB
    - JS: 497.34 kB
    - Total gzipped: ~158 kB
    
    Acesse em: https://brayan4624.github.io/TCC-2025/

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// substitua 'usuario' e 'nexus-app' pelo seu usuário e nome do repositório
-export default defineConfig({
-  base: '/TCC-2025/', 
-  plugins: [react()],
-})
